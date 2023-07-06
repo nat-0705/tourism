e6658b68168ad56b980b0786e82f50d7db2a1a51
takennnusernames <apaleclint@gmail.com>
2023-07-06 12:41:26 +0800

    change book form date setting

--- index.php
+++ index.php
@@ -1,41 +1,41 @@
 <!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
 <?php require_once('config.php'); ?>
- <?php require_once('inc/header.php') ?>
-  <body class="hold-transition layout-top-nav" >
-     <?php $page = isset($_GET['page']) ? $_GET['page'] : 'portal';  ?>
-     <?php require_once('inc/topBarNav.php') ?>
-     <?php 
-        if(!file_exists($page.".php") && !is_dir($page)){
-            include '404.html';
-        }else{
-          if(is_dir($page))
-            include $page.'/index.php';
-          else
-            include $page.'.php';
+<?php require_once('inc/header.php') ?>
 
-        }
-      ?>
-       <script>
-        $(function(){
-          if($('header.masthead').lengt <= 0)
-            $('#mainNav').addClass('navbar-shrink')
-        })
-      </script>
-      <?php require_once('inc/footer.php') ?>
-      <div class="modal fade text-dark" id="confirm_modal" role='dialog'>
+<body class="hold-transition layout-top-nav">
+  <?php $page = isset($_GET['page']) ? $_GET['page'] : 'portal';  ?>
+  <?php require_once('inc/topBarNav.php') ?>
+  <?php
+  if (!file_exists($page . ".php") && !is_dir($page)) {
+    include '404.html';
+  } else {
+    if (is_dir($page))
+      include $page . '/index.php';
+    else
+      include $page . '.php';
+  }
+  ?>
+  <script>
+    $(function() {
+      if ($('header.masthead').lengt <= 0)
+        $('#mainNav').addClass('navbar-shrink')
+    })
+  </script>
+  <?php require_once('inc/footer.php') ?>
+  <div class="modal fade text-dark" id="confirm_modal" role='dialog'>
     <div class="modal-dialog modal-md modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
-        <h5 class="modal-title">Confirmation</h5>
-      </div>
-      <div class="modal-body">
-        <div id="delete_content"></div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
+          <h5 class="modal-title">Confirmation</h5>
+        </div>
+        <div class="modal-body">
+          <div id="delete_content"></div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
       </div>
     </div>
   </div>
@@ -43,14 +43,14 @@
     <div class="modal-dialog modal-md modal-dialog-centered" role="document">
       <div class="modal-content rounded-0">
         <div class="modal-header">
-        <h5 class="modal-title"></h5>
-      </div>
-      <div class="modal-body">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
+          <h5 class="modal-title"></h5>
+        </div>
+        <div class="modal-body">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        </div>
       </div>
     </div>
   </div>
@@ -58,23 +58,24 @@
     <div class="modal-dialog modal-full-height  modal-md" role="document">
       <div class="modal-content">
         <div class="modal-header">
-        <h5 class="modal-title"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span class="fa fa-arrow-right"></span>
-        </button>
-      </div>
-      <div class="modal-body">
-      </div>
+          <h5 class="modal-title"></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span class="fa fa-arrow-right"></span>
+          </button>
+        </div>
+        <div class="modal-body">
+        </div>
       </div>
     </div>
   </div>
   <div class="modal fade text-dark" id="viewer_modal" role='dialog'>
     <div class="modal-dialog modal-md" role="document">
       <div class="modal-content">
-              <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
-              <img src="" alt="">
+        <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
+        <img src="" alt="">
       </div>
     </div>
-  </div> 
-  </body>
+  </div>
+</body>
+
 </html>
--- login.php
+++ login.php
@@ -1,16 +1,17 @@
 <style>
-    #uni_modal .modal-content>.modal-footer,#uni_modal .modal-content>.modal-header{
-        display:none;
+    #uni_modal .modal-content>.modal-footer,
+    #uni_modal .modal-content>.modal-header {
+        display: none;
     }
 </style>
 <div class="container-fluid">
     <h3 class="float-left">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
+            <span aria-hidden="true">&times;</span>
         </button>
     </h3>
     <div class="row">
-        <div class="col-lg-5 border-right">
+        <div class="col">
             <h3 class="text-center">Login</h3>
             <hr>
             <form action="" id="login-form">
@@ -23,104 +24,106 @@
                     <input type="password" class="form-control form" name="password" required>
                 </div>
                 <div class="form-group d-flex justify-content-end">
-                    <button class="btn btn-primary btn-flat">Login</button>
-                </div>
-            </form>
-        </div>
-        <div class="col-lg-7">
-        <h3 class="text-center">Create New Account</h3>
-        <hr class='border-primary'>
-            <form action="" id="registration">
-                <div class="form-group">
-                    <label for="" class="control-label">Firstname</label>
-                    <input type="text" class="form-control form-control-sm form" name="firstname" required>
-                </div>
-                <div class="form-group">
-                    <label for="" class="control-label">Lastname</label>
-                    <input type="text" class="form-control form-control-sm form" name="lastname" required>
-                </div>
-                <div class="form-group">
-                    <label for="" class="control-label">Username</label>
-                    <input type="text" class="form-control form-control-sm form" name="username" required>
-                </div>
-                <div class="form-group">
-                    <label for="" class="control-label">Password</label>
-                    <input type="password" class="form-control form-control-sm form" name="password" required>
-                </div>
-                <div class="form-group d-flex justify-content-end">
-                    <button class="btn btn-primary btn-flat">Register</button>
+                    <button class="btn btn-primary btn-flat m-3">Login</button>
+                    <button class="btn btn-secondary btn-flat m-3">Create account</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
+
+<div class="col-lg-7">
+    <h3 class="text-center">Create New Account</h3>
+    <hr class='border-primary'>
+    <form action="" id="registration">
+        <div class="form-group">
+            <label for="" class="control-label">Firstname</label>
+            <input type="text" class="form-control form-control-sm form" name="firstname" required>
+        </div>
+        <div class="form-group">
+            <label for="" class="control-label">Lastname</label>
+            <input type="text" class="form-control form-control-sm form" name="lastname" required>
+        </div>
+        <div class="form-group">
+            <label for="" class="control-label">Username</label>
+            <input type="text" class="form-control form-control-sm form" name="username" required>
+        </div>
+        <div class="form-group">
+            <label for="" class="control-label">Password</label>
+            <input type="password" class="form-control form-control-sm form" name="password" required>
+        </div>
+        <div class="form-group d-flex justify-content-end">
+            <button class="btn btn-primary btn-flat">Register</button>
+        </div>
+    </form>
+</div>
 <script>
-    $(function(){
-        $('#registration').submit(function(e){
+    $(function() {
+        $('#registration').submit(function(e) {
             e.preventDefault();
             start_loader()
-            if($('.err-msg').length > 0)
+            if ($('.err-msg').length > 0)
                 $('.err-msg').remove();
             $.ajax({
-                url:_base_url_+"classes/Master.php?f=register",
-                method:"POST",
-                data:$(this).serialize(),
-                dataType:"json",
-                error:err=>{
+                url: _base_url_ + "classes/Master.php?f=register",
+                method: "POST",
+                data: $(this).serialize(),
+                dataType: "json",
+                error: err => {
                     console.log(err)
-                    alert_toast("an error occured",'error')
+                    alert_toast("an error occured", 'error')
                     end_loader()
                 },
-                success:function(resp){
-                    if(typeof resp == 'object' && resp.status == 'success'){
-                        alert_toast("Account succesfully registered",'success')
-                        setTimeout(function(){
+                success: function(resp) {
+                    if (typeof resp == 'object' && resp.status == 'success') {
+                        alert_toast("Account succesfully registered", 'success')
+                        setTimeout(function() {
                             location.reload()
-                        },2000)
-                    }else if(resp.status == 'failed' && !!resp.msg){
+                        }, 2000)
+                    } else if (resp.status == 'failed' && !!resp.msg) {
                         var _err_el = $('<div>')
-                            _err_el.addClass("alert alert-danger err-msg").text(resp.msg)
+                        _err_el.addClass("alert alert-danger err-msg").text(resp.msg)
                         $('#registration').prepend(_err_el)
                         end_loader()
-                        
-                    }else{
+
+                    } else {
                         console.log(resp)
-                        alert_toast("an error occured",'error')
+                        alert_toast("an error occured", 'error')
                         end_loader()
                     }
                 }
             })
         })
-        $('#login-form').submit(function(e){
+        $('#login-form').submit(function(e) {
             e.preventDefault();
             start_loader()
-            if($('.err-msg').length > 0)
+            if ($('.err-msg').length > 0)
                 $('.err-msg').remove();
             $.ajax({
-                url:_base_url_+"classes/Login.php?f=login_user",
-                method:"POST",
-                data:$(this).serialize(),
-                dataType:"json",
-                error:err=>{
+                url: _base_url_ + "classes/Login.php?f=login_user",
+                method: "POST",
+                data: $(this).serialize(),
+                dataType: "json",
+                error: err => {
                     console.log(err)
-                    alert_toast("an error occured",'error')
+                    alert_toast("an error occured", 'error')
                     end_loader()
                 },
-                success:function(resp){
-                    if(typeof resp == 'object' && resp.status == 'success'){
-                        alert_toast("Login Successfully",'success')
-                        setTimeout(function(){
+                success: function(resp) {
+                    if (typeof resp == 'object' && resp.status == 'success') {
+                        alert_toast("Login Successfully", 'success')
+                        setTimeout(function() {
                             location.reload()
-                        },2000)
-                    }else if(resp.status == 'incorrect'){
+                        }, 2000)
+                    } else if (resp.status == 'incorrect') {
                         var _err_el = $('<div>')
-                            _err_el.addClass("alert alert-danger err-msg").text("Incorrect Credentials.")
+                        _err_el.addClass("alert alert-danger err-msg").text("Incorrect Credentials.")
                         $('#login-form').prepend(_err_el)
                         end_loader()
-                        
-                    }else{
+
+                    } else {
                         console.log(resp)
-                        alert_toast("an error occured",'error')
+                        alert_toast("an error occured", 'error')
                         end_loader()
                     }
                 }
