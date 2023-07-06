2b06dd231e2ba7d33516105f073b56e5fe6d3062
Nathaniel Gabionza <75109995+nat-0705@users.noreply.github.com>
2023-07-06 22:05:26 +0800

    remove the registration and login

--- register.php
+++ register.php
@@ -77,40 +77,5 @@
                 }
             })
         })
-        $('#login-form').submit(function(e) {
-            e.preventDefault();
-            start_loader()
-            if ($('.err-msg').length > 0)
-                $('.err-msg').remove();
-            $.ajax({
-                url: _base_url_ + "classes/Login.php?f=login_user",
-                method: "POST",
-                data: $(this).serialize(),
-                dataType: "json",
-                error: err => {
-                    console.log(err)
-                    alert_toast("an error occured", 'error')
-                    end_loader()
-                },
-                success: function(resp) {
-                    if (typeof resp == 'object' && resp.status == 'success') {
-                        alert_toast("Login Successfully", 'success')
-                        setTimeout(function() {
-                            location.reload()
-                        }, 2000)
-                    } else if (resp.status == 'incorrect') {
-                        var _err_el = $('<div>')
-                        _err_el.addClass("alert alert-danger err-msg").text("Incorrect Credentials.")
-                        $('#login-form').prepend(_err_el)
-                        end_loader()
-
-                    } else {
-                        console.log(resp)
-                        alert_toast("an error occured", 'error')
-                        end_loader()
-                    }
-                }
-            })
-        })
     })
 </script>
